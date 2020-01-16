c1f3912bc516436d19c4bc8abd4c4098c847a2e8
Jonathan Souza <s2it_josouza@uolinc.com>
2020-01-16 12:04:26 -0300

    restore all

--- graph_banking/web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule GraphBanking.PageController do
-  use GraphBanking.Web, :controller
-
-  def index(conn, _params) do
-    render conn, "index.html"
-  end
-end
--- graph_banking/web/views/account_view.ex
+++
@@ -1,3 +0,0 @@
-defmodule GraphBanking.AccountView do
-  use GraphBanking.Web, :view
-end
