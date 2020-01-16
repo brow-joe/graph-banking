cadd4205d5275b2428b155dcfed8221ac117e871
Jonathan Souza <s2it_josouza@uolinc.com>
2020-01-16 12:10:45 -0300

    add account model

---
+++ web/models/account.ex
@@ -0,0 +1,18 @@
+defmodule GraphBanking.Account do
+  use GraphBanking.Web, :model
+
+  schema "accounts" do
+    field :current_balance, :decimal
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:current_balance])
+    |> validate_required([:current_balance])
+  end
+end
